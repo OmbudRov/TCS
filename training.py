133106ac2ecc359dd49e5218f956d4d4d4dff123
AgentAss <meatius2222@gmail.com>
2023-03-04 13:42:28 +0530

    Setting up Model Storage and Sumo Configuration

--- training.py
+++ training.py
@@ -1,5 +1,9 @@
 import argparse
+import sys
+import os
+import datetime
 
+from sumolib import checkBinary
 def parse_args() -> argparse.Namespace:
     """Takes arguments from command line."""
     Parser=argparse.ArgumentParser()
@@ -14,3 +18,36 @@ def parse_args() -> argparse.Namespace:
 
 if __name__ == "__main__":
     args=parse_args()
+    
+    #Setting up cmd command to run sumo during simulation
+    if 'SUMO_HOME' in os.environ:
+        tools=os.path.join(os.environ['SUMO_HOME'],'tools')
+        sys.path.append(tools)
+    else:
+        sys.exit("'SUMO_HOME' does not exist")
+    if args.gui == True:
+        sumoBinary=checkBinary('sumo')
+    else:
+        sumoBinary=checkBinary('sumo-gui')
+    # "sumoBinary" decides whether to use a GUI
+    # "-c" loads the named config on startup
+    # "os.path.join()" sets up the location of the sumo config file
+    # "--no-step-logging" disables console output of current simulation step
+    # "--waiting-time-memory" is the length of time interval, over which accumulated waiting time is taken into account
+    # "str()" sets the maximum amount of steps allowed in the simulation
+    sumo_cmd=[sumoBinary,"-c",os.path.join('Junction','Config.sumocfg'),"--no-step-logging","--waiting-time-memory",str(args.max_steps)]  
+    
+    #Setting up the Directory for trained models
+    ModelPath=os.path.join(os.getcwd(),"Models",'')
+    os.makedirs(os.path.dirname(ModelPath),exist_ok=True) #Makes a Directory called "Models" if it already doesnt exist
+    Dir=os.listdir(ModelPath)
+    if Dir:
+        versions=[int(name.split("_")[1]) for name in Dir]
+        new_verion=str(max(versions)+1)
+    else:
+        new_verion='1'
+    DataPath=os.path.join(ModelPath,'Model_'+new_verion,'')
+    os.makedirs(os.path.dirname(DataPath),exist_ok=True) #Makes a Directory for the current model being trained
+    
+    Episode=0
+    Start_TimeStamp=datetime.datetime.now()
