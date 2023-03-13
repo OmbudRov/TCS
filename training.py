cec0015add0e0ee9b20a892f686a149a84d05800
AgentAss <meatius2222@gmail.com>
2023-03-14 01:48:02 +0530

    Working a bit on storage of data as graphs

--- training.py
+++ training.py
@@ -1,23 +1,37 @@
+# Handles the main loop that starts an episode on every iteration
+
 import argparse
 import sys
 import os
 import datetime
 
 from model import Model
+from utilities import Visualization
 
 from sumolib import checkBinary
 def parse_args() -> argparse.Namespace:
     """Takes arguments from command line."""
     Parser=argparse.ArgumentParser()
     
+    # Misc arguments
     Parser.add_argument('--mode',help='Choose between making a new model and working further on an existing model',choices=['normal','retraining'],default='normal')
     Parser.add_argument('--gui',help='GUI display option',type=bool,default=False)
     Parser.add_argument('--episodes',help='Total Number of Episodes to train the model on',default=10)
     Parser.add_argument('--max_steps',help='Max Number of steps that can be taken',type=int,default=5400)
     Parser.add_argument('--n_cars',help='Number of cars to be generated in each episode',type=int,default=1000)
     
+    # Model arguments
     Parser.add_argument('--num_layers',help='Number of Layers in the Nueral Network',type=int,default=5)
     Parser.add_argument('--width_layers',help='Dimensionality of the Output Space',type=int,default=400)
+    Parser.add_argument('--batch_size',help='Dimensionality of the Output Space',type=int,default=100)
+    Parser.add_argument('--learning_rate',help='Dimensionality of the Output Space',type=float,default=0.001)
+    Parser.add_argument('--num_states',help='Shape of the Inner Layers of the Nueral Network',type=int,default=80)
+    Parser.add_argument('--num_actions',help='Output Shape of the Nueral Network',type=int,default=4)
+    
+    # Visualization arguments
+    Parser.add_argument('--dpi',type=int,default=100)
+    
+    
     return Parser.parse_args()
 
 if __name__ == "__main__":
@@ -56,4 +70,8 @@ if __name__ == "__main__":
     Episode=0
     Start_TimeStamp=datetime.datetime.now() # To Show the starting time when the program is done executing
     if(args.mode=='normal'):
-        Model=Model(args.num_layers,args.width_layers)
+        # Initialising the Model
+        Model=Model(args.num_layers,args.width_layers,args.batch_size,args.learning_rate,args.num_states,args.num_actions)
+        
+        # Graphs and Stuff
+        Visualization=Visualization(DataPath,args.dpi)
---
+++ utilities.py
@@ -0,0 +1,9 @@
+# Graphs and Stuff
+
+import matplotlib.pyplot as plt
+import os
+
+class Visualization:
+    def __init__(self,path,dpi):
+        self.path=path
+        self.dpi=dpi # Resolution in "Dots Per Inch"
